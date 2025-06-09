2ba5078a8c690d72bbcd6d15abe30d38780fa427
vovamoar <vova.l@moar.tools>
2025-06-09 15:25:53 +0300

    added prettier

--- lesson-console.js
+++ lesson-console.js
@@ -1,19 +1,19 @@
-console.log('External JS file loaded')
+console.log('External JS file loaded');
 
-console.group('Group of console logs')
-console.log('1')
-console.log('2')
-console.log('3')
-console.log('4')
-console.groupEnd()
+console.group('Group of console logs');
+console.log('1');
+console.log('2');
+console.log('3');
+console.log('4');
+console.groupEnd();
 
-console.error('This is an error')
+console.error('This is an error');
 
-console.warn('This is a warning')
+console.warn('This is a warning');
 
-console.table({ name: 'John', age: 30, city: 'New York' })
+console.table({ name: 'John', age: 30, city: 'New York' });
 
 const styles =
-	'background-color: blue; color: white; padding: 5px; border-radius: 5px'
+  'background-color: blue; color: white; padding: 5px; border-radius: 5px';
 
-console.log('%cHello World', styles)
+console.log('%cHello World', styles);
